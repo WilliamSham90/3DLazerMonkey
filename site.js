0da4299100696d8b15b682990e0a765396562ced
WilliamSham90 <89131882+WilliamSham90@users.noreply.github.com>
2023-10-03 14:52:35 +0200

    Add files via upload

---
+++ site.js
@@ -0,0 +1,409 @@
+﻿//Creator of this site : William Sham
+
+
+
+
+//==========================================
+
+(function (window, document) {
+
+    'use strict';
+
+    // patch CustomEvent to allow constructor creation (IE/Chrome)
+    if (typeof window.CustomEvent !== 'function') {
+
+        window.CustomEvent = function (event, params) {
+
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+
+            var evt = document.createEvent('CustomEvent');
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            return evt;
+        };
+
+        window.CustomEvent.prototype = window.Event.prototype;
+    }
+
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchmove', handleTouchMove, false);
+    document.addEventListener('touchend', handleTouchEnd, false);
+
+    var xDown = null;
+    var yDown = null;
+    var xDiff = null;
+    var yDiff = null;
+    var timeDown = null;
+    var startEl = null;
+
+    /**
+     * Fires swiped event if swipe detected on touchend
+     * @param {object} e - browser event object
+     * @returns {void}
+     */
+    function handleTouchEnd(e) {
+
+        // if the user released on a different target, cancel!
+        if (startEl !== e.target) return;
+
+        var swipeThreshold = parseInt(getNearestAttribute(startEl, 'data-swipe-threshold', '20'), 10); // default 20px
+        var swipeTimeout = parseInt(getNearestAttribute(startEl, 'data-swipe-timeout', '500'), 10);    // default 500ms
+        var timeDiff = Date.now() - timeDown;
+        var eventType = '';
+        var changedTouches = e.changedTouches || e.touches || [];
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) { // most significant
+            if (Math.abs(xDiff) > swipeThreshold && timeDiff < swipeTimeout) {
+                if (xDiff > 0) {
+                    eventType = 'swiped-left';
+                }
+                else {
+                    eventType = 'swiped-right';
+                }
+            }
+        }
+        else if (Math.abs(yDiff) > swipeThreshold && timeDiff < swipeTimeout) {
+            if (yDiff > 0) {
+                eventType = 'swiped-up';
+            }
+            else {
+                eventType = 'swiped-down';
+            }
+        }
+
+        if (eventType !== '') {
+
+            var eventData = {
+                dir: eventType.replace(/swiped-/, ''),
+                touchType: (changedTouches[0] || {}).touchType || 'direct',
+                xStart: parseInt(xDown, 10),
+                xEnd: parseInt((changedTouches[0] || {}).clientX || -1, 10),
+                yStart: parseInt(yDown, 10),
+                yEnd: parseInt((changedTouches[0] || {}).clientY || -1, 10)
+            };
+
+            // fire `swiped` event event on the element that started the swipe
+            startEl.dispatchEvent(new CustomEvent('swiped', { bubbles: true, cancelable: true, detail: eventData }));
+
+            // fire `swiped-dir` event on the element that started the swipe
+            startEl.dispatchEvent(new CustomEvent(eventType, { bubbles: true, cancelable: true, detail: eventData }));
+        }
+
+        // reset values
+        xDown = null;
+        yDown = null;
+        timeDown = null;
+    }
+
+    /**
+     * Records current location on touchstart event
+     * @param {object} e - browser event object
+     * @returns {void}
+     */
+    function handleTouchStart(e) {
+
+        // if the element has data-swipe-ignore="true" we stop listening for swipe events
+        if (e.target.getAttribute('data-swipe-ignore') === 'true') return;
+
+        startEl = e.target;
+
+        timeDown = Date.now();
+        xDown = e.touches[0].clientX;
+        yDown = e.touches[0].clientY;
+        xDiff = 0;
+        yDiff = 0;
+    }
+
+    /**
+     * Records location diff in px on touchmove event
+     * @param {object} e - browser event object
+     * @returns {void}
+     */
+    function handleTouchMove(e) {
+
+        if (!xDown || !yDown) return;
+
+        var xUp = e.touches[0].clientX;
+        var yUp = e.touches[0].clientY;
+
+        xDiff = xDown - xUp;
+        yDiff = yDown - yUp;
+    }
+
+    /**
+     * Gets attribute off HTML element or nearest parent
+     * @param {object} el - HTML element to retrieve attribute from
+     * @param {string} attributeName - name of the attribute
+     * @param {any} defaultValue - default value to return if no match found
+     * @returns {any} attribute value or defaultValue
+     */
+    function getNearestAttribute(el, attributeName, defaultValue) {
+
+        // walk up the dom tree looking for attributeName
+        while (el && el !== document.documentElement) {
+
+            var attributeValue = el.getAttribute(attributeName);
+
+            if (attributeValue) {
+                return attributeValue;
+            }
+
+            el = el.parentNode;
+        }
+
+        return defaultValue;
+    }
+
+}(window, document));
+
+//=========================================
+
+
+//=========================================
+
+(function ($) {
+    "use strict";
+    $.fn.sliderResponsive = function (settings) {
+
+        var set = $.extend(
+            {
+                slidePause: 5000,
+                fadeSpeed: 800,
+                autoPlay: "on",
+                showArrows: "off",
+                hideDots: "off",
+                hoverZoom: "on",
+                titleBarTop: "off"
+            },
+            settings
+        );
+
+        var $slider = $(this);
+        var size = $slider.find("> div").length; //number of slides
+        var position = 0; // current position of carousal
+        var sliderIntervalID; // used to clear autoplay
+        
+       
+
+        // Add a Dot for each slide
+        $slider.append("<ul></ul>");
+        $slider.find("> div").each(function () {
+            $slider.find("> ul").append('<li></li>');
+        });
+
+        // Put .show on the first Slide
+        $slider.find("div:first-of-type").addClass("show");
+
+        // Put .showLi on the first dot
+        $slider.find("li:first-of-type").addClass("showli")
+
+        //fadeout all items except .show
+        $slider.find("> div").not(".show").fadeOut();
+
+        // If Autoplay is set to 'on' than start it
+        if (set.autoPlay === "on") {
+            startSlider();
+        }
+
+        // If showarrows is set to 'on' then don't hide them
+        if (set.showArrows === "on") {
+            $slider.addClass('showArrows');
+        }
+
+        // If hideDots is set to 'on' then hide them
+        if (set.hideDots === "on") {
+            $slider.addClass('hideDots');
+        }
+
+        // If hoverZoom is set to 'off' then stop it
+        if (set.hoverZoom === "off") {
+            $slider.addClass('hoverZoomOff');
+        }
+
+        // If titleBarTop is set to 'on' then move it up
+        if (set.titleBarTop === "on") {
+            $slider.addClass('titleBarTop');
+        }
+
+        // function to start auto play
+        function startSlider() {
+            sliderIntervalID = setInterval(function () {
+                nextSlide();
+            }, set.slidePause);
+        }
+
+        // on mouseover stop the autoplay
+        $slider.mouseover(function () {
+            if (set.autoPlay === "on") {
+                clearInterval(sliderIntervalID);
+            }
+        });
+
+        // on mouseout starts the autoplay
+        $slider.mouseout(function () {
+            if (set.autoPlay === "on") {
+                startSlider();
+            }
+        });
+
+        //on right arrow click
+        $slider.find("> .right").click(nextSlide)
+        $slider.on("swiped-left", function () {
+            nextSlide();
+            
+        });
+
+        //on left arrow click
+        $slider.find("> .left").click(prevSlide);
+        $slider.on("swiped-right", function () {
+            prevSlide();
+        });
+
+
+
+        // Go to next slide
+        function nextSlide() {
+            position = $slider.find(".show").index() + 1;
+            if (position > size - 1) position = 0;
+            changeCarousel(position);
+        }
+
+        // Go to previous slide
+        function prevSlide() {
+            position = $slider.find(".show").index() - 1;
+            if (position < 0) position = size - 1;
+            changeCarousel(position);
+        }
+
+        //when user clicks slider button
+        $slider.find(" > ul > li").click(function () {
+            position = $(this).index();
+            changeCarousel($(this).index());
+        });
+
+        //this changes the image and button selection
+        function changeCarousel() {
+            $slider.find(".show").removeClass("show").fadeOut();
+            $slider
+                .find("> div")
+                .eq(position)
+                .fadeIn(set.fadeSpeed)
+                .addClass("show");
+            // The Dots
+            $slider.find("> ul").find(".showli").removeClass("showli");
+            $slider.find("> ul > li").eq(position).addClass("showli");
+        }
+
+        return $slider;
+    };
+})(jQuery);
+
+
+
+//////////////////////////////////////////////
+// Activate each slider - change options
+//////////////////////////////////////////////
+// Using default everything
+            // slidePause: 5000,
+            // fadeSpeed: 800,
+            // autoPlay: "on",
+            // showArrows: "off", 
+            // hideDots: "off", 
+            // hoverZoom: "on", 
+            // titleBarTop: "off"
+$(document).ready(function () {
+    const isTouchSupported = "ontouchstart" in window || window.navigator.maxTouchPoints;
+
+    if (isTouchSupported) {
+        //If mobile divice
+        $("#slider1").sliderResponsive({
+            fadeSpeed: 450,
+            autoPlay: "on",
+            showArrows: "on",
+            hideDots: "on"
+        });
+    } else {
+        //If PC
+        $("#slider1").sliderResponsive({
+            fadeSpeed: 450,
+            autoPlay: "off",
+            showArrows: "on",   
+        });
+    }
+
+    if (isTouchSupported) {
+        $("#slider2").sliderResponsive({
+            fadeSpeed: 450,
+            autoPlay: "on",
+            showArrows: "on",
+            hideDots: "on"
+        });
+    } else {
+        $("#slider2").sliderResponsive({
+            fadeSpeed: 450,
+            autoPlay: "off",
+            showArrows: "on",
+            //hideDots: "on"
+        }); }
+
+});
+
+//Particals
+window.onload = function () {
+    Particles.init({
+        selector: ".background"
+    });
+};
+const particles = Particles.init({
+    selector: ".background",
+    color: ["#03dac6", "#ff0266", "#000000"],
+    connectParticles: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            options: {
+                color: ["#faebd7", "#03dac6", "#ff0266"],
+                maxParticles: 43,
+                connectParticles: false
+            }
+        }
+    ]
+});
+
+
+
+//Super cool about page
+
+let mode = "css"
+const editor = CodeMirror.fromTextArea(code, {
+  lineNumbers: true,
+  styleActiveLine: true,
+  matchBrackets: true,
+  scrollbarStyle: "overlay",
+  Tab: "indentMore",
+  defaultTab: function(cm) {
+    if (cm.somethingSelected()) cm.indentSelection("add");
+    else cm.replaceSelection("  ", "end");
+  },
+  mode
+}) 
+editor.setOption("theme", "highcontrast-dark")
+const xq = document.querySelector(".code");
+const ro = new ResizeObserver(entries => {
+  editor.setSize(xq.offsetWidth, xq.offsetHeight)
+})
+ro.observe(document.querySelector(".code-container"))
+
+const changeMode = (btn) => {
+  mode = {
+    css: "javascript",
+    javascript: "markdown",
+    markdown: "htmlmixed",
+    htmlmixed: "css"
+  }[mode]
+  btn.title = `(click to change) Current Mode: ${mode}`
+  editor.setOption("mode", mode)
+}
+
+//more particals
+
+particlesJS("particles-js", {"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":100,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);
